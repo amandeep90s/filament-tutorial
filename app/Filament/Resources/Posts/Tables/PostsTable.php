34a564c183af509d01933372b5955190f3986488
Amandeep Singh <65186944+amandeep90s@users.noreply.github.com>
2026-04-28 17:31:31 +0530

    feat: Table Actions in Defth

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -2,10 +2,16 @@
 
 namespace App\Filament\Resources\Posts\Tables;
 
+use App\Models\Post;
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ReplicateAction;
+use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DatePicker;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
@@ -48,7 +54,26 @@ class PostsTable
                     ->preload()
             ])
             ->recordActions([
+                Action::make("Status")
+                    ->label("Change Status")
+                    ->icon(Heroicon::ClipboardDocumentCheck)
+                    ->schema([
+                        Checkbox::make('is_published')
+                    ])
+                    ->fillForm(fn (Post $record) => [
+                        'is_published' => $record->is_published,
+                    ])
+                    ->action(function (array $data, Post $record) {
+                        $record->is_published = $data['is_published'];
+                        $record->save();
+                    })
+                ,
+                ReplicateAction::make()
+                    ->beforeReplicaSaved(function ($replica) {
+                        $replica->slug = $replica->slug . '-copy-' . now()->timestamp;
+                    }),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
