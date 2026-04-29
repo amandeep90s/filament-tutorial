71d2ff8295cb9f286cfe412425da6e712041a585
Amandeep Singh <65186944+amandeep90s@users.noreply.github.com>
2026-04-29 16:37:18 +0530

    feat: Chart Widgets

---
+++ app/Filament/Widgets/UserPieWidget.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+
+class UserPieWidget extends ChartWidget
+{
+    protected ?string $heading = 'User Pie Widget';
+
+    protected function getData(): array
+    {
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Users created',
+                    'data' => [300, 50, 100],
+                    'backgroundColor' => [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)'
+                    ]
+                ]
+            ],
+            "labels" => ['India', 'Usa', 'Canada'],
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'pie';
+    }
+}
