b43d95451362c3f3613493cd5c172c874163f665
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-04-30 18:31:11 +0530

    feat: Customize Notifications in FilamentPHP

--- filament-basics/app/Filament/Resources/Users/Pages/CreateUser.php
+++ filament-basics/app/Filament/Resources/Users/Pages/CreateUser.php
@@ -3,9 +3,25 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Support\Icons\Heroicon;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title($this->getCreatedNotificationMessage())
+            ->success()
+            ->icon(Heroicon::OutlinedCheckCircle)
+            ->send();
+    }
+
+    protected function getCreatedNotificationMessage(): ?string
+    {
+        return 'User created successfully.';
+    }
 }
--- filament-basics/app/Filament/Resources/Users/Pages/EditUser.php
+++ filament-basics/app/Filament/Resources/Users/Pages/EditUser.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\DeleteAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Icons\Heroicon;
 
 class EditUser extends EditRecord
 {
@@ -13,7 +15,28 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            DeleteAction::make()
+            ->successNotification(
+                Notification::make()
+                    ->title('User deleted successfully.')
+                    ->body('The user has been deleted.')
+                    ->icon(Heroicon::OutlinedCheckCircle)
+                    ->success()
+            )
         ];
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title($this->getSavedNotificationMessage())
+            ->success()
+            ->icon(Heroicon::OutlinedCheckCircle)
+            ->send();
+    }
+
+    protected function getSavedNotificationMessage(): ?string
+    {
+        return 'User updated successfully.';
     }
 }
